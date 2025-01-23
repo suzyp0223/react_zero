5e3ff21502ced48ac8f9fbeb7b4701ee597866cf
suzy <suzyp0223@gmail.com>
2025-01-23 11:22:11 +0900

    유저id로 title, body 가져오기

--- my-blog/src/components/ReactDocPage.js
+++ my-blog/src/components/ReactDocPage.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 export default function ReactDocPage() {
   //docId:  1, 2, 3...
   const params = useParams();
-  console.log(params);
+  // console.log("params", params);
+  const id = params.docId;
+  const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    async function fetchInfo() {
+      const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      const answer = await result.json();
+      // console.log('result: ', result);
+      return answer;
+    }
+
+    fetchInfo().then((res) => {
+      setInfo(res);
+    });
+  }, [])
+
 
   // navigate()는 url나 링크로 가지않고 프로그래밍적으로 라우팅 처리시 사용하는 함수.
   // 보통 네비게이트는 레이아웃 컴포넌트에 많이 씀.
@@ -14,6 +30,11 @@ export default function ReactDocPage() {
     <>
       <h5 onClick={() => navigate("/")}>logo</h5>
       <div>ReactDocPage  ##{params.docId}</div>
+      <div>Title  ##{info.title}</div>
+      <div>Body  ##{info.body}</div>
+      <div>
+
+      </div>
     </>
   )
 }
--- my-blog/src/components/ReactPage.js
+++ my-blog/src/components/ReactPage.js
@@ -1,51 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
+// API호출은 useEffect로 마운트된 직후 딱한번만 호출되게 코드변경
 export default function ReactPage() {
   // api call -> react에 해당하는 글의 목록을 응답 받음.
+  const [docs, setDocs] = useState([]);
 
-  const docs = [
-    {
-      id: 1,
-      title: "React Study!",
-      data: "05/01/2021",
-    },
-    {
-      id: 2,
-      title: "JPA 시작하기",
-      data: "06/01/2021",
-    },
-    {
-      id: 3,
-      title: "나만의 Todo List App",
-      data: "07/01/2021",
-    },
-    {
-      id: 4,
-      title: "CRA 없이 리액트 프로젝트 시작하기",
-      data: "08/25/2021",
-    },
-    {
-      id: 5,
-      title: "html/css 완전 정복!!!!!",
-      data: "09/01/2025",
-    },
-    {
-      id: 6,
-      title: "마크업의 모든 것",
-      data: "10/01/2021",
-    },
-    {
-      id: 7,
-      title: "JavaScript의 신",
-      data: "05/01/2022",
-    },
-    {
-      id: 8,
-      title: "이제는 사용해보자 useMemo & useCallback",
-      data: "05/16/2024",
-    },
-  ];
+
+  // const docs = [
+  //   {
+  //     id: 1,
+  //     title: "React Study!",
+  //     data: "05/01/2021",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "JPA 시작하기",
+  //     data: "06/01/2021",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "나만의 Todo List App",
+  //     data: "07/01/2021",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "CRA 없이 리액트 프로젝트 시작하기",
+  //     data: "08/25/2021",
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "html/css 완전 정복!!!!!",
+  //     data: "09/01/2025",
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "마크업의 모든 것",
+  //     data: "10/01/2021",
+  //   },
+  //   {
+  //     id: 7,
+  //     title: "JavaScript의 신",
+  //     data: "05/01/2022",
+  //   },
+  //   {
+  //     id: 8,
+  //     title: "이제는 사용해보자 useMemo & useCallback",
+  //     data: "05/16/2024",
+  //   },
+  // ];
+
+
+
+
+
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+      const result = await res.json();
+      // console.log('result: ', result);
+      // setDocs(result);
+      return result;
+    }
+
+    fetchData().then((res) => {
+      setDocs(res);
+    });
+  }, [])
 
   return (
     <div>
